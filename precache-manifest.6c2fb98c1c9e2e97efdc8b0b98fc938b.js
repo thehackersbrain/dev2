a73b4cd69cde18441e916f9c449dc99ef2905493
Gaurav Raj <techw803@gmail.com>
2022-07-11 18:28:44 +0530

    Initial Commit

---
+++ precache-manifest.6c2fb98c1c9e2e97efdc8b0b98fc938b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad832e949d5d5d829978",
+    "url": "/static/css/main.aef7b325.chunk.css"
+  },
+  {
+    "revision": "ad832e949d5d5d829978",
+    "url": "/static/js/main.37e28030.chunk.js"
+  },
+  {
+    "revision": "92a315b10214df5e6b11",
+    "url": "/static/js/runtime~main.fee8973d.js"
+  },
+  {
+    "revision": "51646f4cc059b4f19357",
+    "url": "/static/js/2.658e0faf.chunk.js"
+  },
+  {
+    "revision": "469ae0393c9eca53ea73",
+    "url": "/static/js/3.268c5232.chunk.js"
+  },
+  {
+    "revision": "fe2474349cceafc0c516c8837af8619d",
+    "url": "/static/media/me.fe247434.jpg"
+  },
+  {
+    "revision": "91b07025cd23e244e8d9f226693d734a",
+    "url": "/static/media/bg2.91b07025.jpg"
+  },
+  {
+    "revision": "2471fda27ee6386b80be6b3784203ea8",
+    "url": "/index.html"
+  }
+];
